5f021d0954c9c4a4968b0d25f758514cc8f307f9
gabrielfmendesm <gabrielfmm@al.insper.edu.br>
2025-05-21 22:07:00 -0300

    feat: account

---
+++ src/main/java/store/account/AccountOut.java
@@ -0,0 +1,11 @@
+package store.account;
+
+import lombok.Builder;
+import lombok.experimental.Accessors;
+
+@Builder @Accessors(fluent = true)
+public record AccountOut(
+    String id,
+    String name,
+    String email
+) { }
